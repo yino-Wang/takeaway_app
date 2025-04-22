0b6eb78d7bd2b3b6e5cb1aa75096c1dfa7484fcf
yino-Wang <yinooo66688@gmail.com>
2025-04-22 22:41:50 +1000

    chore: remove node_modules from repo

---
+++ src/components/Cart/index.js
@@ -0,0 +1,91 @@
+import classNames from 'classnames'
+import Count from '../Count'
+import './index.scss'
+import { useDispatch, useSelector } from 'react-redux'
+import { decreCount, increCount } from '../../store/modules/takeaway'
+import { useState } from 'react'
+
+
+const Cart = () => {
+  const { cartList } = useSelector(state => state.foods)
+  // calculate totalprice
+  const totalPrice = cartList.reduce((a, c) => a + c.price * c.count, 0)
+  const dispatch = useDispatch();
+  //
+  const [visible,setVisible] = useState(false)
+  function onShow(){
+    if(cartList.length > 0){
+      setVisible(true)
+    }
+  }
+  return (
+    <div className="cartContainer">
+      {/* 遮罩层 添加visible类名可以显示出来 */}
+      <div
+        className={classNames('cartOverlay',visible && 'visible')}
+          onClick={() => setVisible(false) }
+      />
+      <div className="cart">
+        {/* fill 添加fill类名可以切换购物车状态*/}
+        {/* 购物车数量 */}
+        <div onClick={onShow} className={classNames('icon', cartList.length > 0 && 'fill')}>
+          {cartList.length > 0 && <div className="cartCornerMark">{cartList.length}</div>}
+        </div>
+        {/* 购物车价格 */}
+        <div className="main">
+          <div className="price">
+            <span className="payableAmount">
+              <span className="payableAmountUnit">¥</span>
+              {totalPrice.toFixed(2)}
+            </span>
+          </div>
+          <span className="text">预估另需配送费 ¥5</span>
+        </div>
+        {/* 结算 or 起送 */}
+        {cartList.length>0 ? (
+          <div className="goToPreview">去结算</div>
+        ) : (
+          <div className="minFee">¥20起送</div>
+        )}
+      </div>
+      {/* 添加visible类名 div会显示出来 */}
+      <div className={classNames('cartPanel', visible && 'visible')}>
+        <div className="header">
+          <span className="text">购物车</span>
+          <span className="clearCart">
+            清空购物车
+          </span>
+        </div>
+
+        {/* 购物车列表 */}
+        <div className="scrollArea">
+          {cartList.map(item => {
+            return (
+              <div className="cartItem" key={item.id}>
+                <img className="shopPic" src={item.picture} alt="" />
+                <div className="main">
+                  <div className="skuInfo">
+                    <div className="name">{item.name}</div>
+                  </div>
+                  <div className="payableAmount">
+                    <span className="yuan">¥</span>
+                    <span className="price">{item.price}</span>
+                  </div>
+                </div>
+                <div className="skuBtnWrapper btnGroup">
+                  <Count
+                    count={item.count}
+                    onPlus={() => dispatch(increCount({id: item.id}))}
+                    onMinus={() => dispatch(decreCount({id: item.id}))}
+                  /> 
+                </div>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Cart
---
+++ src/store/modules/takeaway.js
@@ -0,0 +1,65 @@
+import axios from "axios";
+
+const { createSlice } = require("@reduxjs/toolkit");
+
+
+
+const createStore = createSlice({
+    name: 'foods',
+    initialState: {
+        // foods list
+        foodsList: [],
+        activeIndex: 0,
+        cartList: []
+    },
+    reducers: {
+        setFoodsList(state, action){
+            state.foodsList = action.payload;
+        },
+        setActiveIndex(state,action){
+            state.activeIndex = action.payload;
+        },
+        setCartList(state, action) {
+            const item = state.cartList.find(i => i.id === action.payload.id);
+            if (item) {
+              item.count += 1;
+            } else {
+              // 初始化 count 为 1（关键点）
+              state.cartList.push({ ...action.payload, count: 1 });
+            }
+          },
+          
+    // count增
+    increCount (state, action) {
+        // 关键点：找到当前要修改谁的count id
+        const item = state.cartList.find(item => item.id === action.payload.id)
+        item.count++
+      },
+      // count减
+      decreCount (state, action) {
+        // 关键点：找到当前要修改谁的count id
+        const item = state.cartList.find(item => item.id === action.payload.id)
+        if (item.count === 0) {
+          return
+        }
+        item.count--
+      },
+      // 清除购物车
+      clearCart (state) {
+        state.cartList = []
+      }
+    }
+})
+
+const{setFoodsList,setActiveIndex,setCartList, increCount, decreCount, clearCart } = createStore.actions;
+const url = "http://localhost:3004/takeaway";
+const fetchList = () => {
+    return async (dispatch) => {
+        const res = await axios.get(url);
+        dispatch(setFoodsList(res.data));
+    }
+}
+
+export { fetchList, setActiveIndex, setCartList, increCount, decreCount, clearCart}
+const reducer = createStore.reducer;
+export default reducer 
